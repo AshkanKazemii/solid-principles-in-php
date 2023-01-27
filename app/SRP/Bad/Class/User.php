dd232cf1a9b69d91dbbe5242a54ed0cd35edc4ba
ashkan kazemi <ashkan.kazemi.pln@gmail.com>
2023-01-27 21:53:08 +0330

    Bad Code in SRP

---
+++ app/SRP/Bad/Class/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\SRP\Bad\Class ;
+
+
+class User 
+{
+    public function create()
+    {
+        // create user
+    }
+
+    public function delete()
+    {
+        // delete user
+    }
+}
+
---
+++ app/SRP/Bad/Method/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\SRP\Bad\Method ;
+
+
+class User 
+{
+    public function create()
+    {
+        // request : validate data
+
+        // query : create user
+
+        //return or redirect route 
+    }
+
+    public function delete()
+    {
+        // request : validate data
+
+        // query : delete user
+
+        //return or redirect route 
+    }
+}
