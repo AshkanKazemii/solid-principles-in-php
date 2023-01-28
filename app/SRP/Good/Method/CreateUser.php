c9d281580b0b983b3c9572701e67ede53449c85a
ashkan kazemi <ashkan.kazemi.pln@gmail.com>
2023-01-28 11:56:33 +0330

    first update : Good Code in SRP

--- app/SRP/Good/Method/CreateUser.php
+++ app/SRP/Good/Method/CreateUser.php
@@ -5,7 +5,6 @@ namespace App\SRP\Good\Method ;
 use App\Repositories\SRP\Good\CreateNewUserRepo ;
 use App\Request\SRP\Good\DeleteUserRequest ;
 
-include_once __DIR__ . "/../../../../vendor/autoload.php" ;
 
 // Does not respect clean code
 class CreateUser
